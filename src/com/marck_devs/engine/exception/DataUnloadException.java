f7707e03a5983fbfe40910cf5fb260eec97f8ce4
Marck C. Guzmán <marck.guzman.dev@gmail.com>
2020-07-06 11:53:35 +0200

    Created the project exceptions

---
+++ src/com/marck_devs/engine/exception/DataUnloadException.java
@@ -0,0 +1,5 @@
+package com.marck_devs.engine.exception;
+
+public class DataUnloadException extends Exception {
+
+}
---
+++ src/com/marck_devs/engine/exception/EmptyKeyException.java
@@ -0,0 +1,4 @@
+package com.marck_devs.engine.exception;
+
+public class EmptyKeyException extends Exception {
+}
